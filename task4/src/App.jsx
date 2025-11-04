916017fd8879d511baba0aa2aa5a21bac8a02236
Mounika-Thirupathi <thirupathim848@gmail.com>
2025-11-04 13:44:59 +0530

    task4

---
+++ task4/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+function App() {
+
+
+  return (
+    <div className="para">
+      <div>
+        <ol className="para1">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>others</li>
+        </ol>
+        </div>
+        <div className="para2">
+          <section>Birthday Greeting</section>
+          <p>Happy Birthday</p>
+        </div>
+     
+
+    </div>
+  )
+}
+
+export default App
